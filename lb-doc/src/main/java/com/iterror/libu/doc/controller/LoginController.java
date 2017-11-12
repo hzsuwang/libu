0678b1325a4223d0222b82c2b54d8c791b09f1f3
hzsuwang <hzsuwang@163.com>
2017-11-12 22:20:28 +0800

    项目中增加时间程序

--- lb-doc/src/main/java/com/iterror/libu/doc/controller/LoginController.java
+++ lb-doc/src/main/java/com/iterror/libu/doc/controller/LoginController.java
@@ -1,9 +1,38 @@
 package com.iterror.libu.doc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iterror.libu.common.bto.ResultBTO;
 import com.iterror.libu.common.view.BaseController;
 
 /**
  * Created by tony.yan on 2017/11/5.
  */
+@RestController
 public class LoginController extends BaseController {
+
+    @RequestMapping(value = "/member/login.json", method = RequestMethod.POST)
+    @ResponseBody
+    public ResultBTO login() {
+        ResultBTO resultBTO = new ResultBTO();
+        return resultBTO;
+    }
+
+    @RequestMapping(value = "/member/get_vcode.json", method = RequestMethod.POST)
+    @ResponseBody
+    public ResultBTO getVcode() {
+        ResultBTO resultBTO = new ResultBTO();
+        return resultBTO;
+    }
+
+    @RequestMapping(value = "/member/check_vcode.json", method = RequestMethod.POST)
+    @ResponseBody
+    public ResultBTO checkVcode() {
+        ResultBTO resultBTO = new ResultBTO();
+        return resultBTO;
+    }
+
 }
